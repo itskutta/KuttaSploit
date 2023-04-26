16e78ac7dd8335d5d62f73c3b67c0d1e917d5ad7
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-26 11:20:23 +0530

    Create kutta.sh

---
+++ kutta.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Author : itskutta
+#Copyright (c) : itskutta
+#Script :
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+clear
+echo "Welcome To Kutta Sploit"
+sleep 3
+echo "${RED}This script is only for educational purposes , i am not reliable for any damage caused by this script to anyone. Use it on your on .${ENDCOLOR}"
+sleep 3
+echo "Bye"
