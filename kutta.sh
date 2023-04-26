637bf512ba6562d4a29b422147fdcb4e2484cbb9
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-26 15:18:41 +0530

    Update kutta.sh

--- kutta.sh
+++ kutta.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
+
 #Author : itskutta
+
 #Copyright (c) : itskutta
+
 #Script :
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
+
 clear
+
+echo " ##:::'##:'##::::'##:'########:'########::::'###::::"
+
+echo " ##::'##:: ##:::: ##:... ##..::... ##..::::'## ##:::"
+
+echo " ##:'##::: ##:::: ##:::: ##::::::: ##:::::'##:. ##::"
+
+echo " #####:::: ##:::: ##:::: ##::::::: ##::::'##:::. ##:"
+
+echo " ##. ##::: ##:::: ##:::: ##::::::: ##:::: #########:"
+
+echo " ##:. ##:: ##:::: ##:::: ##::::::: ##:::: ##.... ##:"
+
+echo " ##::. ##:. #######::::: ##::::::: ##:::: ##:::: ##:"
+
+echo " ..::::..:::.......::::::..::::::::..:::::..:::::..::"
+
+sleep 5
+
+RED="\e[31m"
+
+GREEN="\e[32m"
+
+ENDCOLOR="\e[0m"
+
+clear
+
 echo "Welcome To Kutta Sploit"
+
 sleep 3
-echo "${RED}This script is only for educational purposes , i am not reliable for any damage caused by this script to anyone. Use it on your on .${ENDCOLOR}"
+
+clear
+
+echo -e "${RED}This script is only for educational purposes , i am not reli>
+
 sleep 3
+
 echo "Bye"
